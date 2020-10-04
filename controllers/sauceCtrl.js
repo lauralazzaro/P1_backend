9509018b33406585babd3b5db44df303a0abaadf
Laura <lz_laura@hotmail.com>
2020-10-04 18:30:09 +0200

    0.2.0 - delete one

--- controllers/sauceCtrl.js
+++ controllers/sauceCtrl.js
@@ -1,26 +1,22 @@
 // handles CRUD operation for sauces
 const Sauce = require('../models/sauceModel');
+const fs = require('fs');
 
 //SEARCH FOR ALL SAUCES IN DB
 exports.getAllSauces = (req, res) => {
     Sauce.find().then(
-        (sauces) => {
-            res.status(200).json(sauces);
-        }
+        (sauces) => { res.status(200).json(sauces); }
     ).catch(
         (error) => {
-            res.status(400).json({
-                error: error
-            });
+            res.status(400).json({ error: error });
         }
     );
 };
 
 // SEARCH FOR ONE SPECIFIC SAUCE
 exports.getOneSauce = (req, res) => {
-    Sauce.findOne({
-        _id: req.params.id
-    }).then(
+    Sauce.findOne({ _id: req.params.id }
+    ).then(
         (sauces) => { res.status(200).json(sauces); }
     ).catch(
         (error) => res.status(400).json({ error: error }));
@@ -42,11 +38,22 @@ exports.updateSauce = (req, res) => {
         (error) => res.status(400).json({ error: error }));
 };
 
+// DELETE ONE SAUCE
+exports.deleteSauce = (req, res) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce deleted!'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+};
+
 // CREATE NEW SAUCE IN DB
 exports.createSauce = () => { };
-
-// DELETE ONE SAUCE
-exports.deleteSauce = () => { };
 
 // ADD LIKES OR DISLIKES FROM USERS
 exports.likes = () => { };
