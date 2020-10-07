74b95a9bd8005d2d11e6c345d7c2dec5048196c4
Laura <lz_laura@hotmail.com>
2020-10-07 12:22:52 +0200

    switch not working
    Error: Uncaught (in promise)

--- controllers/sauceCtrl.js
+++ controllers/sauceCtrl.js
@@ -44,12 +44,12 @@ exports.deleteSauce = (req, res) => {
         .then(sauce => {
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
-            Sauce.deleteOne({ _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Sauce deleted!'}))
-            .catch(error => res.status(400).json({ error }));
-        });
-      })
-      .catch(error => res.status(500).json({ error }));
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce deleted!' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 // CREATE NEW SAUCE IN DB
@@ -57,13 +57,60 @@ exports.createSauce = (req, res, next) => {
     const sauce = JSON.parse(req.body.sauce);
     delete sauce._id;
     const newSauce = new Sauce({
-      ...sauce,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        ...sauce,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     newSauce.save()
-      .then(() => res.status(201).json({ message: 'New sauce created!' }))
-      .catch(error => res.status(400).json({ error }));
-  };
+        .then(() => res.status(201).json({ message: 'New sauce created!' }))
+        .catch(error => res.status(400).json({ error }));
+};
 
 // ADD LIKES OR DISLIKES FROM USERS
-exports.likes = () => { };
+exports.likes = (req, res, next) => {
+    const like = req.body.like;
+    const userId = req.body.userId;
+    const sauceId = req.params.id;
+
+    switch (like) {
+        case 1:
+            Sauce.findOne({ _id: sauceId })
+                .then(
+                    Sauce.updateOne(
+                        { _id: sauceId },
+                        {
+                            $inc: { likes: 1 },
+                            $push: { usersLiked: userId }
+                        }
+                    ).then(
+                        () => res.status(200).json({ message: 'Like updated!' })
+                    ).catch(
+                        (error) => res.status(400).json({ error }))
+                ).catch(
+                    (error) => res.status(400).json({ error }));
+            break;
+        case -1:
+            Sauce.updateOne(
+                { _id: sauceId },
+                {
+                    $inc: { dilikes: -1 },
+                    $push: { usersDisiked: userId }
+                }
+            ).then(
+                () => res.status(200).json({ message: 'Disike updated!' })
+            ).catch(
+                (error) => res.status(400).json({ error }));
+            break;
+        case 0:
+            Sauce.updateOne(
+                { _id: sauceId },
+                {
+                    $inc: { likes: -1 },
+                    $pull: { usersLiked: userId }
+                }
+            ).then(
+                () => res.status(200).json({ message: 'Like updated!' })
+            ).catch(
+                (error) => res.status(400).json({ error }));
+            break;
+    };
+};
