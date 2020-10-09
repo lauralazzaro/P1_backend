a8053fe23dbed46be197025313ce81eeb3699bbc
Laura <lz_laura@hotmail.com>
2020-10-09 11:39:18 +0200

    0.2.1 - authorization

--- routes/sauceRoute.js
+++ routes/sauceRoute.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 const sauceCtrl = require('../controllers/sauceCtrl');
 const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
 
-router.get('', sauceCtrl.getAllSauces);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.post('', multer, sauceCtrl.createSauce);
-router.put('/:id', multer, sauceCtrl.updateSauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
-router.post('/:id/like', sauceCtrl.likes);
+router.get('', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likes);
 
 module.exports = router;
 
