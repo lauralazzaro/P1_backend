a636589c62b9ed0f885aa92f7d68bbd375bae28a
Laura <lz_laura@hotmail.com>
2020-10-13 23:30:54 +0200

    Added HTTPS
    removed HTTPS

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,26 +3,43 @@ const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const passwordValidator = require('password-validator');
 require('dotenv/config');
+
+const pwdCheck = new passwordValidator();
+
+pwdCheck
+    .is().min(8)                                    // Minimum length 8
+    .is().max(100)                                  // Maximum length 100
+    .has().uppercase()                              // Must have uppercase letters
+    .has().lowercase()                              // Must have lowercase letters
+    .has().digits(2)                                // Must have at least 2 digits
+    .has().not().spaces();                           // Should not have spaces
 
 // POST : /api/auth/signup 
 exports.signup = (req, res, next) => {
 
-    const emailCipher = crypto.createCipheriv('aes-256-cbc', process.env.USER_KEY, process.env.USER_IV);
-    const emailStr = emailCipher.update(req.body.email, 'utf8', 'hex') + emailCipher.final('hex');
+    if (pwdCheck.validate(req.body.password)) {
+        const emailCipher = crypto.createCipheriv('aes-256-cbc', process.env.USER_KEY, process.env.USER_IV);
+        const emailStr = emailCipher.update(req.body.email, 'utf8', 'hex') + emailCipher.final('hex');
 
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
 
-            const user = new User({
-                email: emailStr,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: 'User created!' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+                const user = new User({
+                    email: emailStr,
+                    password: hash
+                });
+                user.save()
+                    .then(() => res.status(201).json({ message: 'User created!' }))
+                    .catch(error => res.status(400).json({ error }));
+            })
+            .catch(error => res.status(500).json({ error }));
+
+    } else {
+        return false;
+    }
+
 };
 
 // POST : /api/auth/login 
--- server.js
+++ server.js
@@ -42,9 +42,9 @@ const server = http.createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
 server.listen(port);
