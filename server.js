739a0788579ca7b7e684ec7483e65292c143add2
Laura <lz_laura@hotmail.com>
2020-10-13 23:30:54 +0200

    Added HTTPS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
+const fs = require('fs');
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -38,13 +39,17 @@ const errorHandler = error => {
     }
 };
 
-const server = http.createServer(app);
+const server = https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: process.env.PEM_PWD
+}, app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
 server.listen(port);
