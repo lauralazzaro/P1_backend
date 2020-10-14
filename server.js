61b1bfbc9476b9415147cf8a025955903d8b1e08
Laura <lz_laura@hotmail.com>
2020-10-14 12:49:18 +0200

    refactoring no console

--- server.js
+++ server.js
@@ -26,10 +26,12 @@ const errorHandler = error => {
     const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
     switch (error.code) {
         case 'EACCES':
+            // eslint-disable-next-line no-console
             console.error(bind + ' requires elevated privileges.');
             process.exit(1);
             break;
         case 'EADDRINUSE':
+            // eslint-disable-next-line no-console
             console.error(bind + ' is already in use.');
             process.exit(1);
             break;
@@ -44,6 +46,7 @@ server.on('error', errorHandler);
 server.on('listening', () => {
     const address = server.address();
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    // eslint-disable-next-line no-console
     console.log('Listening on ' + bind);
 });
 
